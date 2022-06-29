1d64a3a7daaa6880e3041ea9b24236bb030436a0
Dejen <dejen@Zipcoders-MacBook-Pro-88.local>
2022-06-29 11:50:44 -0400

    adding remove char method

--- src/main/java/StringUtilities.java
+++ src/main/java/StringUtilities.java
@@ -47,8 +47,8 @@ public class StringUtilities {
      * @return `value` with char of value `charToRemove` removed
      */
     public String removeCharacter(String value, Character charToRemove) {
-//        return value.replace(charToRemove, "");
-      return null;
+       String valueNew = value.replaceAll(String.valueOf(charToRemove), "");
+      return valueNew;
     }
 
     /**
